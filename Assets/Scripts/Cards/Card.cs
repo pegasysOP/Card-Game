b537559ac5ca414bb33accfd547b072b77a6f4e2
pegasys <sconnolly7@outlook.com>
2023-10-27 17:43:09 +0100

    started groundwork for card deck

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Card : MonoBehaviour
 {
     [SerializeField] private CardBase cardBase;
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private Deck deck = new Deck();
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    [SerializeField] private Deck deck;
 
-    // Update is called once per frame
-    void Update()
+    // for testing
+    [SerializeField] private List<Card> cardList;
+
+    private void Start()
     {
-        
+        deck = new Deck(cardList);
     }
 }
