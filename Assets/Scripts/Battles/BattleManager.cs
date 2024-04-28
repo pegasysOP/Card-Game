a761e00a2c0591343ac6c61358fc9e00df34d213
pegasys <sconnolly7@outlook.com>
2024-04-28 17:18:16 +0100

    completed base match loop (no card playing)

--- Assets/Scripts/Battles/BattleManager.cs
+++ Assets/Scripts/Battles/BattleManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleManager : MonoBehaviour
 {
+    // TODO: this should be changed to be a neater, non direct way of referencing the button
+    [SerializeField] private Button endTurnButton;
+
     private BattleState battleState = BattleState.None;
     public BattleState BattleState { get { return battleState; } }
 
@@ -15,6 +19,11 @@ public class BattleManager : MonoBehaviour
 
     private List<Card> playerHand;
     private List<Card> opponentHand;
+
+    private void Awake()
+    {
+        endTurnButton.onClick.AddListener(OnEndTurnButtonClicked);
+    }
 
     public void StartBattle(Deck playerDeck, Deck opponentDeck)
     {
@@ -52,10 +61,48 @@ public class BattleManager : MonoBehaviour
     private IEnumerator BeginPlayerTurn()
     {
         playerTurn = true;
-        battleState = BattleState.Selection;
+        battleState = BattleState.Processing;
 
         EventManager.InvokeMatchEvent(new MatchEventArgs(MatchEventType.PlayerTurnStart));
 
+        List<Card> drawnCards = new List<Card>();
+        playerDeck.Draw(out drawnCards, 1);
+        EventManager.InvokeCardEvent(new CardEventArgs(drawnCards.ToArray(), true, CardEventType.CardDrawn));
+        playerHand.AddRange(drawnCards);
+
+        battleState = BattleState.Selection;
         yield return null;
     }
+
+    private void OnEndTurnButtonClicked()
+    {
+        if (!playerTurn || battleState != BattleState.Selection)
+            return;
+
+        StartCoroutine(BeginOpponentTurn());
+    }
+
+    private IEnumerator BeginOpponentTurn()
+    {
+        playerTurn = false;
+        battleState = BattleState.Processing;
+
+        EventManager.InvokeMatchEvent(new MatchEventArgs(MatchEventType.EnemyTurnStart));
+
+        List<Card> drawnCards = new List<Card>();
+        opponentDeck.Draw(out drawnCards, 1);
+        EventManager.InvokeCardEvent(new CardEventArgs(drawnCards.ToArray(), false, CardEventType.CardDrawn));
+        opponentHand.AddRange(drawnCards);
+
+        battleState = BattleState.Selection;
+        yield return OpponentCardSelection();
+    }
+
+    private IEnumerator OpponentCardSelection()
+    {
+        // pick opponents moves
+        yield return new WaitForSeconds(3f);
+        yield return BeginPlayerTurn();
+    }
+
 }
