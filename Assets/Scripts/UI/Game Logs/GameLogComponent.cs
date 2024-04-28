b6001e477d1e4d7c9088447f76bd120d8b0648f9
pegasys <sconnolly7@outlook.com>
2024-04-28 13:23:59 +0100

    added the ability for game logs to be multiple lines

--- Assets/Scripts/UI/Game Logs/GameLogComponent.cs
+++ Assets/Scripts/UI/Game Logs/GameLogComponent.cs
@@ -1,15 +1,27 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameLogComponent : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI timeText;
-    [SerializeField] TextMeshProUGUI messageText;
+    [SerializeField] private TextMeshProUGUI timeText;
+    [SerializeField] private TextMeshProUGUI messageText;
+    [SerializeField] private LayoutElement layoutElement;
 
     public void Init(string message)
     {
         timeText.text = DateTime.Now.ToString("HH:mm:ss");
         messageText.text = message;
+        
+        RecalculateHeight();
     }
+
+    private void RecalculateHeight()
+    {
+        float padding = -messageText.rectTransform.sizeDelta.y;
+
+        layoutElement.preferredHeight = messageText.GetPreferredValues().y + padding;
+    }
+
 }
